a49fa397fdde01940b87a019491b2724ddb4d998
Alex Au-Yeung <ah.auyeung@gmail.com>
2014-11-28 17:56:30 +0800

    loading feeds using Google Feed API

--- js/jquery.rssGrid.js
+++ js/jquery.rssGrid.js
@@ -5,24 +5,41 @@
  */
 (function ( $ ) {
 $.fn.rssGrid = function(options){
-	
+	var feed_data =[];
+	var entries = [];
+	var feed_count = 0;
 	var settings = $.extend({
 		"sources": [],
-		"newsCount": 10,
+		"newsCount": 5,
 		"boxWidth" : 300,
-		"containerWidth":"100%"
+		"containerWidth":"100%",
+		"showSource": false
 	}, options);
 	
 	function init(){
-		for(var i=0; i<settings.sources.length; i++){
+		feed_count = settings.sources.length;
+		for(var i=0; i<feed_count; i++){
 			parseRSS(settings.sources[i].url);
 		}
 	}
 	
-	function parseRSS(feed_url){
-		console.log(feed_url);
+	function parseRSS(feed_title, feed_image, feed_url){
+		$.ajax({
+	  		url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num='+settings.newsCount+'&callback=?&q=' + encodeURIComponent(feed_url),
+		    dataType: 'json',
+		    success: function(data) {
+		      //console.log(data.responseData.feed);
+		      feed_data.push(data.responseData.feed);
+		    
+		    }
+	  });
 	}
 	
+	
+	
+	function debugOutput(msg){
+		$("#debug_console").html($("#debug_console").html()+"\n\n&gt;&gt; "+msg);
+	}
 	
 	init();
 	
